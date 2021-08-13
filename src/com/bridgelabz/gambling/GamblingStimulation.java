a85b67de8cc2adae4f9a2c5f18004d0e2088e3e3
rojajullapalli <rojajules@gmail.com>
2021-08-13 08:52:14 +0530

    intializing intialstake and betstake

--- src/com/bridgelabz/gambling/GamblingStimulation.java
+++ src/com/bridgelabz/gambling/GamblingStimulation.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.gambling;
 
 public class GamblingStimulation {
+    public static int IitialStake=100;
+    public static int BetStake=0;
+
     public static void main(String[] args) {
         System.out.println("welcome to gambling stimulation problem");
+        
     }
 }
