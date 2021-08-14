542d956df4cd00fce1c37fc422f013269f998141
rojajullapalli <rojajules@gmail.com>
2021-08-14 19:03:04 +0530

    after 20 days win/lose

--- src/com/bridgelabz/gambling/GamblingStimulation.java
+++ src/com/bridgelabz/gambling/GamblingStimulation.java
@@ -3,34 +3,39 @@ package com.bridgelabz.gambling;
 public class GamblingStimulation {
     public static int STARTINGSTAKE = 100;
     public static final int BETSTAKE = 1;
-    public static final int WIN = 1,LOSE=0;
-    public static final double FIFTY_PERCENT_LOSS=STARTINGSTAKE*0.5;
-    public static final double FIFTY_PERCENT_GAIN=STARTINGSTAKE*1.5;
-    public static String winOrLose()
-    {
+    public static double FIFTY_PERCENT_LOSS=STARTINGSTAKE*0.5;
+    public static double FIFTY_PERCENT_GAIN=STARTINGSTAKE*1.5;
+
+    public static final int DAY=20;
+    public static String winOrLose() {
         double random=(int) Math.floor(Math.random() * 10) % 2;
         if(random==1)
             return "Winning";
         else
             return "Lossing";
     }
-    public static void ResignDay()
-    {
-        while(STARTINGSTAKE>(FIFTY_PERCENT_LOSS)&&STARTINGSTAKE<(FIFTY_PERCENT_GAIN))
-        {
-            if(winOrLose()=="Winning")
-                STARTINGSTAKE+=BETSTAKE;
+    public static void ResignDay() {
+        int loss_amt=0,won_amt=0;
+        while (STARTINGSTAKE > (FIFTY_PERCENT_LOSS) && STARTINGSTAKE < (FIFTY_PERCENT_GAIN)) {
+            if (winOrLose() == "Winning")
+                STARTINGSTAKE += BETSTAKE;
             else
-                STARTINGSTAKE-=BETSTAKE;
-
+                STARTINGSTAKE -= BETSTAKE;
         }
-        if(BETSTAKE>(STARTINGSTAKE*0.5))
-            System.out.println("Gambler loss 50% so resigning day"+STARTINGSTAKE);
-        else
-            System.out.println("Gambler gain 50% so resigning day"+STARTINGSTAKE);
+
+    }
+    public static void twentyDay(){
+        for(int i=1;i<=DAY;i++){
+            ResignDay();
+        }
     }
     public static void main(String[] args) {
         System.out.println("welcome to gambling stimulation problem");
-        ResignDay();
+        twentyDay();
+        if(STARTINGSTAKE>100)
+            System.out.println("Gambler win and total amount = " +STARTINGSTAKE);
+        else
+            System.out.println("Gambler loss and total amount = " +STARTINGSTAKE);
     }
+
 }
